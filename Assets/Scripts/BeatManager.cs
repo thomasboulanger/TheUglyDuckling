d511bbdb330dffbd46963d9140ab294e8436e172
Elootam <toma.lion@hotmail.fr>
2021-10-22 17:05:38 +0200

    debuts

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BeatManager : MonoBehaviour
 {
     public float BPM1;
     public float BPM2;
     public float BPM3;
+    public AudioClip Music0;
     public AudioClip Music1;
     public AudioClip Music2;
     public AudioClip Music3;
@@ -20,25 +22,89 @@ public class BeatManager : MonoBehaviour
     private AudioSource _audioSource;
     private float _beatTimer;
     private float _beatInterval;
+    private float _volume;
+    private float _rhythmStacks;
+
+    public enum enumScene
+    {
+        MainMenu,
+        Level1,
+        Level2,
+        Level3
+    }
+
+    public enumScene currentScene;
+
+
+
     void Start()
     {
         _bpm1 = BPM1;
         _bpm2 = BPM2;
         _bpm3 = BPM3;
         _audioSource = GetComponent<AudioSource>();
-        _audioSource.PlayOneShot(Music1,1f);
+        _volume = GetComponent<AudioSource>().volume;
+        
     }
 
     void Update()
     {
-        _beatTimer += Time.deltaTime;
-        _beatInterval = 60 / BPM1;
-        if (_beatTimer >= _beatInterval)
+        if (SceneManager.GetActiveScene().buildIndex != 0)
         {
-            _beatTimer -= _beatInterval;
-            _audioSource.PlayOneShot(Music2,1f);
+            _beatTimer += Time.deltaTime;
         }
-        transform.position = new Vector3(0f,_beatTimer,0f);
-
+        else
+        {
+            _beatTimer = 0f;
+        }
+        
+        switch (SceneManager.GetActiveScene().buildIndex)
+        {
+            case 0:
+                _audioSource.clip = Music0;          
+                _audioSource.Play();
+                break;
+            case 1:
+                _audioSource.clip = Music1;
+                if (!_audioSource.isPlaying)
+                {
+                    _audioSource.Play();
+                }
+                
+                _beatInterval = 60 / _bpm1;
+                if (_beatTimer >= _beatInterval)
+                {
+                    _beatTimer -= _beatInterval;
+                }
+                break;
+            case 2:
+                _audioSource.clip = Music2;          
+                if (!_audioSource.isPlaying)
+                {
+                    _audioSource.Play();
+                }
+                
+                _beatInterval = 60 / _bpm2;
+                if (_beatTimer >= _beatInterval)
+                {
+                    _beatTimer -= _beatInterval;
+                }
+                break;
+            case 03:
+                _audioSource.clip = Music3;          
+                if (!_audioSource.isPlaying)
+                {
+                    _audioSource.Play();
+                }
+                
+                _beatInterval = 60 / _bpm3;
+                if (_beatTimer >= _beatInterval)
+                {
+                    _beatTimer -= _beatInterval;
+                }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
