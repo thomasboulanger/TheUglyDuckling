4aeed1364f1be4cc2439e8f5d5e1aa1d360f9b1a
Elootam <toma.lion@hotmail.fr>
2021-10-23 20:16:55 +0200

    clean du come comme jaja

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -21,9 +21,10 @@ public class BeatManager : MonoBehaviour
     private static float _bpm3;
     private AudioSource _audioSource;
     private float _beatTimer;
-    private float _beatInterval;
+    private static float _beatInterval;
     private float _volume;
     private float _rhythmStacks;
+    private int _sceneIndex;
 
     public enum enumScene
     {
@@ -35,7 +36,10 @@ public class BeatManager : MonoBehaviour
 
     public enumScene currentScene;
 
-
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 
     void Start()
     {
@@ -43,64 +47,62 @@ public class BeatManager : MonoBehaviour
         _bpm2 = BPM2;
         _bpm3 = BPM3;
         _audioSource = GetComponent<AudioSource>();
+        _audioSource.loop = true;
         _volume = GetComponent<AudioSource>().volume;
     }
 
     void Update()
     {
-        if (SceneManager.GetActiveScene().buildIndex != 0)
+        if (_sceneIndex != 0)
         {
             _beatTimer += Time.deltaTime;
         }
-        else
-        {
-            _beatTimer = 0f;
-        }
         
-        switch (SceneManager.GetActiveScene().buildIndex)
+        if (_beatTimer >= _beatInterval)
+        {
+            _beatTimer -= _beatInterval;
+            //do the pulse here
+        }
+    }
+
+    public void GetSceneIndex()
+    {
+        _sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        //for development only
+        if (_sceneIndex > 3)
+        {
+            _sceneIndex = 3;
+        }
+        //end
+        
+        switch (_sceneIndex)
         {
             case 0:
+                currentScene = enumScene.MainMenu;
                 _audioSource.clip = Music0;          
                 _audioSource.Play();
                 break;
+            
             case 1:
+                currentScene = enumScene.Level1;
                 _audioSource.clip = Music1;
-                if (!_audioSource.isPlaying)
-                {
-                    _audioSource.Play();
-                }
-                
+                _audioSource.Play();
                 _beatInterval = 60 / _bpm1;
-                if (_beatTimer >= _beatInterval)
-                {
-                    _beatTimer -= _beatInterval;
-                }
                 break;
+            
             case 2:
-                _audioSource.clip = Music2;          
-                if (!_audioSource.isPlaying)
-                {
-                    _audioSource.Play();
-                }
-                
+                currentScene = enumScene.Level2;
+                _audioSource.clip = Music2;
+                _audioSource.Play();
                 _beatInterval = 60 / _bpm2;
-                if (_beatTimer >= _beatInterval)
-                {
-                    _beatTimer -= _beatInterval;
-                }
                 break;
-            case 03:
-                _audioSource.clip = Music3;          
-                if (!_audioSource.isPlaying)
-                {
-                    _audioSource.Play();
-                }
-                
+            
+            case 3:
+                currentScene = enumScene.Level3;
+                _audioSource.clip = Music3;
+                _audioSource.Play();
                 _beatInterval = 60 / _bpm3;
-                if (_beatTimer >= _beatInterval)
-                {
-                    _beatTimer -= _beatInterval;
-                }
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
--- Assets/Scripts/SceneEventScript.cs
+++ Assets/Scripts/SceneEventScript.cs
@@ -1,24 +1,11 @@
-
-using System;
-using System.Security.Cryptography.X509Certificates;
-using System.Security.Policy;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.SceneManagement;
 
 public class SceneEventScript : MonoBehaviour
 {
-    private UnityEvent _sceneEvent = new UnityEvent();
-    public static int _sceneIndex;
+    public UnityEvent _sceneEvent = new UnityEvent();
     void Start()
     {
-        _sceneIndex = SceneManager.GetActiveScene().buildIndex;
-        _sceneEvent.AddListener(SceneLoad);
         _sceneEvent.Invoke();
-    }
-
-    public void SceneLoad()
-    {
-        Debug.Log(_sceneIndex);
     }
 }
