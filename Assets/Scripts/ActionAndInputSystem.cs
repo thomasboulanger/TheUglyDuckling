18c5a68525dbb6eb4ab096745804310855e0045d
Elootam <toma.lion@hotmail.fr>
2021-11-15 14:23:37 +0100

    reparer les merde de jurdan

--- Assets/Scripts/ActionAndInputSystem.cs
+++ Assets/Scripts/ActionAndInputSystem.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using Scenes.Jordan.Scripts;
 using UnityEngine;
 
-public class ActionAndInputSystem : MonoBehaviour
+public class ActionAndInputSystem : Entity
 {
     public AudioClip clipUp;
     public AudioClip clipDown;
