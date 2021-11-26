a180b0d3430a905ce91dbd067fedb4a2533d042b
Elootam <toma.lion@hotmail.fr>
2021-11-26 12:11:53 +0100

    +1

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,12 +1,17 @@
+using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject pulsePanel;
+    
     [SerializeField] private Slider healthSlider;
     [SerializeField] private Slider specialHitSlider;
 
     private PlayerManager _player;
+    private Color _tmpColor;
     
     private void Awake()
     {
@@ -16,10 +21,28 @@ public class UIManager : MonoBehaviour
         SetMaxSliderValue(specialHitSlider, Variables.MaxSpecial);
     }
 
+    private void Start()
+    {
+        _tmpColor = pulsePanel.transform.GetChild(0).GetComponent<RawImage>().color;
+    }
+
     private void Update()
     {
         SetCurrentSliderValue(healthSlider, _player.Health);
         SetCurrentSliderValue(specialHitSlider, BeatManager.Stacks);
+        if (BeatManager.pulseAperture)
+        {
+            _tmpColor.a = 1f;
+        }
+        else
+        {
+            _tmpColor.a = 0f;
+        }
+
+        for (int i = 0; i < pulsePanel.transform.childCount; i++)
+        {
+           pulsePanel.transform.GetChild(i).GetComponent<RawImage>().color = _tmpColor;
+        }
     }
 
     private static void SetMaxSliderValue(Slider currentSlider, int maxValue) => currentSlider.maxValue = maxValue;
